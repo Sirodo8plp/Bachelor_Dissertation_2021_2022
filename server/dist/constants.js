b7e354c74e4a740545a94101955c1ddfeb17f6c5
Sirodo8plp <te01234@hotmail.com>
2022-06-13 11:11:40 +0200

    Added files to .gitignore

--- server/dist/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IPINFO_KEY = exports.COOKIE_SECRET = exports.USER_COOKIE_NAME = exports.__prod__ = void 0;
-exports.__prod__ = process.env.NODE_ENV === "production";
-exports.USER_COOKIE_NAME = "qid";
-exports.COOKIE_SECRET = "6c7JxEARZbNjvGJ79P2XZ6EBrw6R3j";
-exports.IPINFO_KEY = "acbd78915c2362";
-//# sourceMappingURL=constants.js.map
